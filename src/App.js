39346ae74b7fb0b67cdfe9e547550dc877055f6b
bebusl <brill_be@naver.com>
2020-09-30 23:46:09 +0900

    4.1  rendering the movies

--- src/App.js
+++ src/App.js
@@ -1,29 +1,44 @@
-import React from 'react';
+import React from "react";
 import axios from "axios";
+import Movie from "./Movie";
 
-
-class App extends React.Component{
+class App extends React.Component {
   state = {
-    isLoading : true,
-    movies : []
+    isLoading: true,
+    movies: [],
   };
 
   getMovies = async () => {
-    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
-    //영화 목록 긁어오기
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    //destruct구조로 원하는 데이터만 들고옴
   };
 
-  componentDidMount() { 
+  componentDidMount() {
     this.getMovies();
     //componentDidMount는 컴포넌트가 첫 렌더링 됐을 때 생성자 호출, 프로펄티 가져오고, 렌더 한 다음 componentDiDMount 실행
   }
-  
 
-  render(){
+  render() {
     const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading...":"We are Ready"}</div>
+    return (
+      <div>
+        {isLoading
+          ? "Loading..."
+          : movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+            ))}
+      </div>
+    );
   }
-
-
-  
 }
---
+++ src/Movie.js
@@ -0,0 +1,17 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Movie({ id, year, title, summary, poster }) {
+  return <h4>{title}</h4>;
+}
+//원래 props로 뭉쳐가지고 인자받는데, destruct구조 쓰면 prop내의 원하는 데이터 정확하게 가져올 수 있음
+
+Movie.propTypes = {
+  id: PropTypes.number.isRequired,
+  year: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+};
+
+export default Movie;
