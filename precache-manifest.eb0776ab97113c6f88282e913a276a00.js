374805790180fd936feb58230f3702fc72171d0a
bebusl <brill_be@naver.com>
2020-10-01 00:51:40 +0900

    Updates

---
+++ precache-manifest.eb0776ab97113c6f88282e913a276a00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea98a8cd5a4bcc6d018a178e58f6f402",
+    "url": "/movie_app_nomad/index.html"
+  },
+  {
+    "revision": "ccbceb61a52fe826855c",
+    "url": "/movie_app_nomad/static/css/main.a3f87dbf.chunk.css"
+  },
+  {
+    "revision": "2cb00f0b337cb1b1485d",
+    "url": "/movie_app_nomad/static/js/2.03729dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_nomad/static/js/2.03729dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccbceb61a52fe826855c",
+    "url": "/movie_app_nomad/static/js/main.d8173178.chunk.js"
+  },
+  {
+    "revision": "e1292ce7e75942e424c8",
+    "url": "/movie_app_nomad/static/js/runtime-main.dc1a9c3b.js"
+  }
+]);
