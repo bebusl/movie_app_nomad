57c61e7386c107e56541ffafd83def7fa9b2462d
bebusl <brill_be@naver.com>
2020-10-01 00:00:56 +0900

    4.2  Styling Movies

--- src/Movie.js
+++ src/Movie.js
@@ -1,8 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css"
 
 function Movie({ id, year, title, summary, poster }) {
-  return <h4>{title}</h4>;
+  return(
+      <div class="movie">
+          <img src={poster} alt={title} title={title} />
+          <div class="movie__data">
+              <h3 class="movie__title">{title}</h3>
+              <h5 class="movie__year">{year}</h5>
+              <p class="movie__summary">{summary}</p>
+          </div>
+      </div>
+  )
 }
 //원래 props로 뭉쳐가지고 인자받는데, destruct구조 쓰면 prop내의 원하는 데이터 정확하게 가져올 수 있음
 
