ee62debee743aa9dbe0eba7e097fc042944c77ed
bebusl <brill_be@naver.com>
2020-10-01 00:17:48 +0900

    4.3 Adding Genres

--- src/Movie.js
+++ src/Movie.js
@@ -1,19 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
-import "./Movie.css"
+import "./Movie.css";
 
-function Movie({ id, year, title, summary, poster }) {
-  return(
-      <div className="movie">
-          <img src={poster} alt={title} title={title} />
-          <div className="movie__data">
-              <h3 className="movie__title">{title}</h3>
-              <h5 className="movie__year">{year}</h5>
-              <p className="movie__summary">{summary}</p>
-          </div>
+function Movie({ id, year, title, summary, poster, genres }) {
+  return (
+    <div className="movie">
+      <img src={poster} alt={title} title={title} />
+      <div className="movie__data">
+        <h3 className="movie__title">{title}</h3>
+        <h5 className="movie__year">{year}</h5>
+        <ul className="genres">
+          {genres.map((genre, index) => (
+            <li className="genres__genre" key={index}>{genre}</li>
+          ))}
+        </ul>
+        <p className="movie__summary">{summary}</p>
       </div>
-  )
+    </div>
+  );
 }
+//map function은 item외에도 item number 인자를 넘겨준다. 보통 index/number 등으로 씀
+//map펑션의 경우 key값이 꼭 필요(구분을 해야하자나)
 //원래 props로 뭉쳐가지고 인자받는데, destruct구조 쓰면 prop내의 원하는 데이터 정확하게 가져올 수 있음
 
 Movie.propTypes = {
@@ -22,6 +29,7 @@ Movie.propTypes = {
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Movie;
